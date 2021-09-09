f34246973faf017e51e0a26301534e686996a998
Rio-Lv <wong.rogerio@gmail.com>
2021-09-09 11:08:23 +0100

    updating input text in build tools

--- src/DB_Build_Tools/Tree/BuildTools.js
+++ src/DB_Build_Tools/Tree/BuildTools.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import TextInput from "./TextInput";
 import { doc, setDoc } from "firebase/firestore";
 import { db } from "../../firebase";
 
@@ -9,9 +8,35 @@ function BuildTools(props) {
   const [add, setAdd] = useState(false);
   const [text, setText] = useState("");
 
+  // make the ids unique
+  const inputId = `${props.path}_${props.id}_input`;
+  const buttonId = `${props.path}_${props.id}_button`;
+
+  // simulate add button click on enter key
   useEffect(() => {
-    console.log(text);
-  }, [text]);
+    var input = document.getElementById(inputId);
+    input.addEventListener("keyup", function (event) {
+      // Number 13 is the "Enter" key on the keyboard
+      if (event.key === "Enter") {
+        // Cancel the default action, if needed
+        console.log(buttonId);
+        event.preventDefault();
+        // Trigger the button element with a click
+        document.getElementById(buttonId).click();
+      }
+    });
+  }, [buttonId, inputId]);
+
+  const createDoc = () => {
+    console.log(props.nextPath);
+    if (text !== "") {
+      const ref = doc(db, props.nextPath, text);
+      setDoc(ref, { info: "cheese info" }, { merge: true });
+    }
+    setAdd(false);
+    setText("");
+    document.getElementById(inputId).value = "";
+  };
 
   useEffect(() => {
     setAdd(props.me);
@@ -52,22 +77,32 @@ function BuildTools(props) {
         }}
       >
         <div>
-          <TextInput
-            show={add}
-            id={props.name + props.index}
-            setText={setText}
-          />
+          <InputBox>
+            <CenterBlock>
+              <Input
+                style={{
+                  width: add ? "160px" : "0px",
+                  opacity: add ? "100%" : "0%",
+                }}
+                type="text"
+                id={inputId}
+                name="project_type_input"
+                onChange={(e) => {
+                  e.preventDefault();
+                  // console.log(e.target.value);
+                  setText(e.target.value);
+                }}
+              />
+            </CenterBlock>
+          </InputBox>
         </div>
         <div
+          id={buttonId}
           onClick={() => {
             if (!add) {
               setAdd(true);
             } else {
-              console.log(props.nextPath);
-              const ref = doc(db, props.nextPath, text);
-              setDoc(ref, { info: "cheese info" }, { merge: true });
-              setAdd(false);
-              setText("");
+              createDoc();
             }
           }}
           style={{
@@ -121,4 +156,28 @@ const EditData = styled.div`
   box-sizing: border-box;
   border: 2px solid white;
   user-select: none;
+`;
+
+const InputBox = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  box-sizing: border-box;
+
+  border-radius: 7px;
+  transition: 0.3s;
+`;
+
+const Input = styled.input`
+  width: 160px;
+  height: 15px;
+  transform: translate(5px, 4px);
+  /* border: 2px solid white; */
+  border-radius: 7px;
+  transition: 0.3s;
+`;
+
+const CenterBlock = styled.div`
+  display: flex;
+  flex-direction: row;
 `;
